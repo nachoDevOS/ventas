7404139636f967d1d2187bc3b2031f4f9a5ae678
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-02-23 10:48:49 -0400

    dev·

---
+++ app/Models/Laboratory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\RegistersUserEvents;
+
+class Laboratory extends Model
+{
+    use HasFactory, RegistersUserEvents, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'name',
+        'observation',
+        'status',
+
+        'registerUser_id',
+        'registerRole',
+        'deleted_at',
+        'deleteUser_id',
+        'deleteRole',
+        'deleteObservation',
+    ];
+}
---
+++ database/migrations/2026_02_23_102904_create_presentations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('presentations', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('code')->nullable();
+            $table->string('name')->nullable();
+            $table->string('observation')->nullable();
+
+
+            $table->smallInteger('status')->default(1);
+
+            $table->timestamps();            
+            $table->foreignId('registerUser_id')->nullable()->constrained('users');
+            $table->string('registerRole')->nullable();
+
+            $table->softDeletes();
+            $table->foreignId('deleteUser_id')->nullable()->constrained('users');
+            $table->string('deleteRole')->nullable();
+            $table->text('deleteObservation')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('presentations');
+    }
+};
