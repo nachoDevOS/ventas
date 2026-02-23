b98e29f724c1e641abd4260ac1cb2427b1e40c22
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-02-23 13:12:10 -0400

    dev

--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -4,8 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\RegistersUserEvents;
 
 class SaleDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, RegistersUserEvents, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'sale_id',
+        'itemStock_id',
+        'itemStockFraction_id',
+        'dispensed', // Entero, Fraccionado
+    
+        'pricePurchase',
+        'presentation_id',
+        'price',
+        'quantity',
+        'amount',
+
+        'registerUser_id',
+        'registerRole',
+        'deleted_at',
+        'deleteUser_id',
+        'deleteRole',
+        'deleteObservation',
+    ];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class,'sale_id');
+    }
+    public function itemStock()
+    {
+        return $this->belongsTo(ItemStock::class,'itemStock_id');
+    }
+    public function itemStockFraction()
+    {
+        return $this->belongsTo(ItemStockFraction::class,'itemStockFraction_id');
+    }
 }
--- app/Models/SaleTransaction.php
+++ app/Models/SaleTransaction.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\RegistersUserEvents;
 
 class SaleTransaction extends Model
 {
-    use HasFactory;
+    use HasFactory, RegistersUserEvents, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'transaction_id',
+        'sale_id',
+        'cashier_id',
+        'paymentType',
+        'amount',
+        'observation',
+        'status',
+
+        'registerUser_id',
+        'registerRole',
+        'deleted_at',
+        'deleteUser_id',
+        'deleteRole',
+        'deleteObservation',
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class, 'sale_id');
+    }
+    public function register()
+    {
+        return $this->belongsTo(User::class, 'registerUser_id')->withTrashed();
+    }
 }
